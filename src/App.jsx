d2be014f96937be69bf11f8528b07f4cce684c94
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-12-01 04:07:03 +0530

    app component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Register from "./components/Register"
+import Register from "./components/SignUp"
+import LandingPage from "./components/LandingPage";
 import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignUpVerify from "./components/SignUpVerify";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () =>{
     return(
         <>
-        <Register/>
-        <Login/>
+        <Header/>
+        <LandingPage/>
+        <Footer/>
         </>
     )
 }
