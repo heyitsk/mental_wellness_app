c22ee5e069650221485d07606ef7a028417f8770
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-12-01 04:08:12 +0530

    Login component created

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import signin from "../assets/signin.png"
+import { Link } from "react-router-dom";
+
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const[passwordVisibility,setPasswordVisibility] = useState(false)
+ 
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,15 +32,20 @@ const Login = () => {
       console.error("Error submitting login:", err);
     }
   };
+  
+  
 
   return (
-    <div className="absolute right-28 top-24 w-1/4 z-30">
-      <div className="p-4 py-10 bg-[#AFFFBB] bg-opacity-35 rounded shadow-md border-2 ">
-        <h1 className="text-2xl font-semibold text-gray-800 text-center mb-6">Login</h1>
+    <div className="absolute right-28 top-10  z-30">
+      
+      <div className="flex flex-col justify-center items-center">
+        <img src={signin} className="relative w-[150px] z-20 top-[35px] h-[80px]"/>
+      <div className="p-4 py-10 bg-[#AFFFBB] bg-opacity-35  rounded-2xl shadow-md border-2 z-10 w-96 h-[400px] ">
+        {/* <h1 className="text-2xl font-semibold text-gray-800 text-center mb-6">Login</h1> */}
         {error && <p className="text-red-500 text-center mb-4">{error}</p>}
         <form onSubmit={handleSubmit} className="">
           <div>
-            <label htmlFor="login-email" className="block text-sm font-medium text-gray-600">
+            <label htmlFor="login-email" className="block font-alice font-normal  text-gray-600">
               Email
             </label>
             <input
@@ -44,31 +56,47 @@ const Login = () => {
               placeholder="Enter your email"
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 my-3"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 my-2"
             />
           </div>
-          <div>
-            <label htmlFor="login-password" className="block text-sm font-medium text-gray-600">
+          <div className="mt-8">
+          <label className=" font-alice font-normal  text-gray-600" htmlFor="password">
               Password
             </label>
+          <div className="flex gap-4 relative">
             <input
-              type="password"
-              id="login-password"
+              type={passwordVisibility?"text":"password"}
+              id="password"
               name="password"
-              placeholder="Enter your password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              // onChange={handleChange}
+              placeholder="Enter your password"
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 mt-2"
               required
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 my-3"
             />
+            <button 
+               type="button"
+                className="absolute right-1 top-4"
+                onClick={()=>{
+                   setPasswordVisibility(!passwordVisibility)
+                   }}>{passwordVisibility?"🔒":"👁️"}</button>
+            </div>
+            </div>
+          
+          <div className="text-xs font-normal text-right my-3">
+            <p><Link to="/forgetpss">Forgot Password?</Link></p>
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2  text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 my-10"
+            className="w-full mt-10 px-4 py-2  text-white font-alice font-normal  bg-[#33D7FF] rounded-sm hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 "
           >
             Login
           </button>
+          <div className="text-center ">
+            <p><Link to="/SignUp">Don’t have an account?</Link></p>
+          </div>
         </form>
+      </div>
       </div>
     </div>
   );
