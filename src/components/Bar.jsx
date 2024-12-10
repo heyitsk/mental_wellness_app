679f00a63a872ed93b98bf41dc01cf908c9916cd
heyitsk <kushagradpr2005@gmail.com>
2024-12-10 12:09:40 +0530

    bar component created

---
+++ src/components/Bar.jsx
@@ -0,0 +1,62 @@
+import { Chart as ChartJS, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+// Register necessary Chart.js components for Bar chart
+ChartJS.register(Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+const BarChart = (info) => {
+    console.log("Info ", info);
+    const { data } = info;
+    console.log("bar chart data", data);
+
+    return (
+        <>
+            <div className="absolute bg-white border-2 border-black rounded-lg left-[200px] inline-block p-4">
+                <div className="w-[800px]">
+                    <Bar
+                        data={{
+                            labels: [
+                                "Emotional Well Being",
+                                "Stress and Anxiety",
+                                "Social Relationships",
+                                "Self Esteem",
+                            ],
+                            datasets: [
+                                {
+                                    label: "Mental Wellness",
+                                    data: [
+                                        data.emotionalWellBeing * 10,
+                                        data.stressAndAnxiety * 10,
+                                        data.socialRelationships * 10,
+                                        data.selfEsteem * 10,
+                                    ],
+                                    backgroundColor: [
+                                        "rgb(255, 99, 132)",
+                                        "rgb(54, 162, 235)",
+                                        "rgb(255, 205, 86)",
+                                        "rgb(75, 192, 192)",
+                                    ],
+                                    borderColor: [
+                                        "rgb(255, 99, 132)",
+                                        "rgb(54, 162, 235)",
+                                        "rgb(255, 205, 86)",
+                                        "rgb(75, 192, 192)",
+                                    ],
+                                    borderWidth: 2,
+                                },
+                            ],
+                        }}
+                        options={{
+                            maintainAspectRatio: false,
+                        }}
+                        height={600}
+                        width={800}
+                        
+                    />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default BarChart;
