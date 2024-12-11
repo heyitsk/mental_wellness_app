61899d00ba5a8c97e2192a3bb9db8e7f4e22a9a1
heyitsk <kushagradpr2005@gmail.com>
2024-12-11 12:26:41 +0530

    recommendation card background upated

--- src/components/RecommendationCard.jsx
+++ src/components/RecommendationCard.jsx
@@ -1,10 +1,13 @@
+import sh3 from "../assets/sh3.jpeg"
+
 const RecommendationCard = ({ title, category,link }) => {
     return (
-      <div className="bg-white shadow-md rounded-lg p-4 my-4 hover:bg-slate-500 cursor-pointer"
+      <div className="bg-[cover] shadow-md rounded-lg p-4 my-4 hover:bg-slate-500 cursor-pointer "
+      style={{ backgroundImage: `url(${sh3})` }}
       onClick={()=>{
         window.open(link)
     }}>
-        <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
+        <h3 className="text-lg  text-gray-800  font-alice font-bold">{title}</h3>
         <p className="text-sm text-gray-600 mt-2">Category: {category}</p>
         
       </div>
