f05cd6948ee35ab80af6ac3ec50c8658cd109761
heyitsk <kushagradpr2005@gmail.com>
2024-12-12 14:57:27 +0530

    journal created

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,7 +1,15 @@
+import paper from "../assets/paper.jpg"
+
 const Journal = () => {
     return(
         <>
-        
+        <p className="absolute left-[200px]  font-alice font-semibold text-[30px]">Start your journal</p>
+        <div className="absolute left-[200px] top-[48px] bg-white p-4 rounded-lg shadow-lg w-[80%] h-[700px]">
+            
+            <form className="h-full">
+                <textarea className="w-[100%] h-full border-2 border-black rounded-lg p-4 text-[25px] font-alice leading-10" ></textarea>
+            </form>
+        </div>
         </>
     )
 }
