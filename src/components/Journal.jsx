ff717773af423832ce9e8fdcb5f3899910216f7f
heyitsk <kushagradpr2005@gmail.com>
2024-12-12 16:17:33 +0530

    submit button added

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -9,7 +9,9 @@ const Journal = () => {
             <form className="h-full">
                 <textarea className="w-[100%] h-full border-2 border-black rounded-lg p-4 text-[25px] font-alice leading-10" ></textarea>
             </form>
+            <button className="p-4 bg-[#33D7FF] border-none rounded-md mt-10 font-alice font-normal px-10">Submit</button>
         </div>
+        
         </>
     )
 }
